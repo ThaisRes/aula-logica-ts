4e0d27a4fdeac84b61c01b9f9784f4cabf5ef9e6
Thais <thaisres@live.com>
2026-07-02 21:31:02 -0300

    feat: list 2 - exercise 17

--- listas/lista02/ex17.ts
+++ listas/lista02/ex17.ts
@@ -2,3 +2,18 @@
 // Enunciado: Login certo: admin, senha: 1234. Se algo estiver errado, diga qual (login ou senha).
 // admin/1234 → ok · admin/0000 → "senha errada" · joao/1234 → "login errado".
 
+const digLogin:string = prompt("Digite o login: ") ?? "0";
+const digSenha:string = prompt("Digite a senha: ") ?? "0";
+const senha:number = Number(digSenha);
+
+if(senha !== 1234 && digLogin !== "admin") {
+    alert(`${digLogin} \ ${senha} -> Login e Senha errados`)
+} else if(senha !== 1234 && digLogin == "admin") {
+    alert(`${digLogin} \ ${senha} -> Senha errada`)
+} else if(senha == 1234 && digLogin !== "admin") {
+    alert(`${digLogin} \ ${senha} -> Login errado`)
+} else if(senha == 1234 && digLogin == "admin") {
+    alert(`${digLogin} \ ${senha} -> ok`)
+} else {
+    alert(`O Login é formado por letras e o senha apenas por números.`)
+}
---
+++ listas/lista02/ex18.ts
@@ -0,0 +1,3 @@
+// Lista 02 — Exercício 18
+// Enunciado: Leia 3 lados. Se formam triângulo (cada lado < soma dos outros 2), classifique: equilátero, isósceles ou escaleno.
+// 5,5,5 → equilátero · 5,5,3 → isósceles · 3,4,5 → escaleno · 1,1,5 → não forma.
